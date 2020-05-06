a9abca7a61ad67d5ee9baad806c744342f12e89b
Jose T. Monagas <jmonagas@localhost.localdomain>
2020-05-06 19:52:27 -0400

    Add footer for desktop

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,18 @@
+<footer class="container-fluid">
+    <div class="row justify-content-between">
+        <div class="col-md-4">
+            <img src="https://via.placeholder.com/300x120?text=Lavatu" alt="Logo Lavatu" />
+        </div>
+        <div class="col-md-2">
+            <a href="" class="display-2">
+                <i class="fab fa-facebook-square"></i>
+            </a>
+            <a href="" class="display-2">
+                <i class="fab fa-instagram-square"></i>
+            </a>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <small class="text-muted text-center">Lavatu. Todos los derechos reservados. {{ date("Y") }}</small>
+    </div>
+</footer>
