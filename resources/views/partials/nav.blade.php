b4841de76e93d0a202337fa845f948507834a838
Jose T. Monagas <jmonagas@localhost.localdomain>
2020-05-06 19:52:12 -0400

    Add Navbar for desktop

---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,19 @@
+            <nav class="row sticky-top">
+                <div class="container">
+                    <div class="col-md">
+                        <div class="card mt-4 static" style="z-index: 2">
+                            <div class="card-body bg-dark rounded shadow d-flex flex-row justify-content-between align-items-baseline">
+                                <div>
+                                    <v-btn class="ma-2" href="#servicio" outlined color="white">Nuestro Servicio</v-btn>
+                                    <v-btn class="ma-2" href="#nosotros" outlined color="white">Conocenos</v-btn>
+                                </div>
+                                <img class="img-fluid" alt="Logo lavatu" src="https://via.placeholder.com/300x120?text=Lavatu" style="position:absolute; margin: -3% 33% 0 36%;" />
+                                <div>
+                                    <v-btn class="ma-2" href="#contacto" outlined color="white">Contactanos</v-btn>
+                                    <v-btn class="ma-2" outlined color="primary">Solicita ya</v-btn>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
